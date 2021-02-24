3ce2a0b4f6bcd6ff0bc77d49c75f755d3d0b51d6
Vitor Tavares <atavares.vitor@gmail.com>
2021-02-24 12:50:23 -0300

    inserido o desafio ao final do countdown

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,10 +1,17 @@
 import { createContext, useState, ReactNode } from 'react'
+import challenges from '../../challenges.json'
 
+interface Challenge {
+    type: string;
+    description: string;
+    amount: number;
+}
 
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
     challengesCompleted: number;
+    activeChallenge: Challenge;
     levelUp: () => void;
     startNewChallenge: () => void;
 }
@@ -21,12 +28,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
     function levelUp() {
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log('New Challenge');
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
     }
 
     return (
@@ -36,7 +48,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 currentExperience, 
                 challengesCompleted, 
                 levelUp,
-                startNewChallenge
+                startNewChallenge,
+                activeChallenge
                 }}
             >
             {children}
