4e8a12abd0b5d724d4e5a78eeaeb4ff426265d45
Vitor Tavares <atavares.vitor@gmail.com>
2021-02-25 11:11:52 -0300

    Ativar notificacao e o audio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react'
+import { createContext, useState, ReactNode, useEffect } from 'react'
 import challenges from '../../challenges.json'
 
 interface Challenge {
@@ -35,15 +35,28 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     const experienceToNextLevel = Math.pow((level + 1) * 3, 2)
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
+
+    
     function levelUp() {
         setLevel(level + 1);
     }
-
+    
     function startNewChallenge() {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
         const challenge = challenges[randomChallengeIndex];
-
+        
         setActiveChallenge(challenge);
+        
+        new Audio('/notification.mp3').play();
+        
+        if (Notification.permission === 'granted') {
+            new Notification('Novo desafio!', {
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge() {
